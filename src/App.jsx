3d9e7c137bcd2a291f43f84e8040735e294d9747
td0wns <trevor.p.downs@gmail.com>
2025-04-08 15:28:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from "react";
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL
-process.env.REACT_APP_SUPABASE_ANON_KEY
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
 );
 
 export default function App() {
